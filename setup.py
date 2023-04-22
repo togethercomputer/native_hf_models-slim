75426bfa2aca61e02b620ffd4c1815220a97e022
Jared Baldridge <jrb@expunge.us>
2023-04-21 18:25:15 -0700

    correct script name

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='native_hf_model',
+    name='native_hf_models',
     version='0.0.1',
     url='https://github.com/togethercomputer/native_hf_model',
     author='Together Computer',
     author_email='support@together.xyz',
     description='This package runs native HF models on the together computer',
     packages=find_packages(),    
-    scripts=['bin/serve_local_nlp_model'],
+    scripts=['bin/serves_local_nlp_model'],
     install_requires=['torch',
         'transformers',
         'together_worker',
